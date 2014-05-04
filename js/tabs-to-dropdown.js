01a13b220e1125d0c07a07ca1f27320655e45a38
ChrisTheButcher <ccwijnia@gmail.com>
2014-05-04 15:40:18 +0200

    prevent default if opac=0

--- js/tabs-to-dropdown.js
+++ js/tabs-to-dropdown.js
@@ -15,19 +15,23 @@ var tabsToList = function() {
             tabListItemOffset    = $(this).offset().left + $(this).outerWidth();
 
         if (tabListItemOffset >= tabbarWidth) {
-            $(this).addClass("hide").find("a").on('click',function(e){e.preventDefault();});
+            $(this).addClass("hide").find("a");
             dropdownListItem.addClass("show");
         } else {
-            $(this).removeClass("hide").find("a").off('click');;
+            $(this).removeClass("hide");
             dropdownListItem.removeClass("show");
         }
     });
-    tabList.children('.hide').length != 0 ?
+    tabList.children(".hide").length != 0 ?
     dropdown.addClass("show") :dropdown.removeClass("show");
 };
 $(window).on("load resize", tabsToList);
 
 $(function(){
+    $("header").find("a,button").click(function(e){
+        if ($(this).css("opacity")==0) e.preventDefault();
+    });
+
     tabListItem.clone().appendTo(dropdownList);
 
     dropdownList.click(function(e) {
