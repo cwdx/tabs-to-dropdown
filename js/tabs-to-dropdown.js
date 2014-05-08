233ddf20c4108734e906b7bb7219bd24f0275651
ChrisTheButcher <ccwijnia@gmail.com>
2014-05-08 19:15:05 +0200

    iOS fix click/touchstart

--- js/tabs-to-dropdown.js
+++ js/tabs-to-dropdown.js
@@ -10,6 +10,8 @@ $(function() {
         var dropdownToggle  = dropdown.children(".dropdown-toggle");
         var dropdownList    = dropdown.children("ul");
 
+        var clickHandler = ("ontouchstart" in document.documentElement ? "touchstart": "click");
+
         var tabsToDropdown  = function() {
             var tabBarWidth = tabBar.width();
             tabListItem.each(function(index) {
@@ -23,21 +25,21 @@ $(function() {
                 }
             });
 
-            tabList.children(".ttd-hide").length != 0 ? dropdown.addClass("ttd-show"): dropdown.removeClass("ttd-show");
+            tabList.children(".ttd-hide").length > 0 ? dropdown.addClass("ttd-show"): dropdown.removeClass("ttd-show");
         };
 
         tabListItem.clone().appendTo(dropdownList);
 
         tabsToDropdown();
         $(window).bind("resize", function(){
-            if(typeof sizeWait != "undefined") { clearTimeout(sizeWait) };
+            if(typeof sizeWait != "undefined") { clearTimeout(sizeWait); }
             sizeWait = setTimeout(function(){
                 tabsToDropdown();
             },resizeTimeout);
         });
 
-        dropdown.on("touchstart click", function(e) { e.stopPropagation() });
-        dropdownToggle.on("touchstart click", function(e) { dropdown.toggleClass("ttd-open"); e.stopPropagation() });
-        $(document).on("touchstart click", function() { dropdown.removeClass("ttd-open") });
+        dropdown.bind(clickHandler, function(e) { e.stopPropagation(); });
+        $(document).bind(clickHandler, function() { dropdown.removeClass("ttd-open"); });
+        dropdownToggle.bind(clickHandler, function(e) { dropdown.toggleClass("ttd-open"); e.stopPropagation(); });
     });
 });
